f42cff8e076ce62f15a2c976ef375f1f830ed145
WonkiJo <joweonki@gmail.com>
2020-04-02 15:49:39 +0900

    Update Gas Station.kt

--- Gas Station.kt
+++ Gas Station.kt
@@ -1,1 +1,21 @@
-
+class Solution {
+    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+        var station = 0
+        var checker = 0
+        var total = 0
+        
+        gas.forEachIndexed { index, value -> 
+            val diff = value - cost[index]
+            
+            checker += diff
+            total += diff
+            
+            if(checker < 0) {
+                checker = 0
+                station = index + 1
+            }
+        }
+        
+        return if(total >= 0) station else -1
+    }
+}
